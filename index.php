bba0b4617cdbd776358e466c289649f52f5313ba
acgguelas <112476015+acgguelas@users.noreply.github.com>
2022-08-30 17:53:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -31,11 +31,6 @@
     <link rel = "stylesheet" href = "style-2.css">
   </head>
   <body>
-    <?php
-    session_start();
-    if (!isset($_SESSION['user_name'])) {
-    ?>
-    //coomment
     <!-- Navigation Bar -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top">
       <a class="navbar-brand" href="index.php"></a>
@@ -67,47 +62,6 @@
         </ul>
       </div>
     </nav>
-  <?php
-  } elseif (isset($_SESSION['user_name'])) {
-  ?>
-    <!-- Navigation Bar -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
-      <a class="navbar-brand" href="index.php"></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <a href="index.php"><img src="images/logo.png" style="width:80px;height:42px;margin-left:20%;" class="logo_enable"></a>
-        <ul class="navbar-nav ml-auto">
-          <li class="d-lg-none">
-              <a href="index.php" class="text d-lg-none">Home</a>
-              <hr class="d-lg-none" />
-            </li>
-          <li class="nav-item d-none d-lg-block"><a class="nav-link" href="index.php">Home</a></li>
-          <li class="d-lg-none">
-              <a href="about.php" class="text d-lg-none">About</a>
-              <hr class="d-lg-none" />
-            </li>
-          <li class="nav-item d-none d-lg-block"><a class="nav-link" href="about.php">About</a></li>
-          <li class="d-lg-none">
-              <a href="products.php" class="text d-lg-none">Products</a>
-              <hr class="d-lg-none" />
-            </li>
-          <li class="nav-item d-none d-lg-block"><a class="nav-link" href="products.php">Products</a></li>
-          <li class="d-lg-none">
-              <a href="faq.php" class="text d-lg-none">FAQ</a>
-              <hr class="d-lg-none" />
-            </li>
-          <li class="nav-item d-none d-lg-block"><a class="nav-link" href="faq.php">FAQ</a></li>
-          
-            </div>
-          </div>
-        </ul>
-      </div>
-    </nav>
-  <?php
-  }
-  ?>
- 
- 
  <div class="container-fluid" id="containerhome" style="margin-top: 150px; color: white;">
 <h2>CICS - Student Welfare and Development Board</h2>
 <br/> 
